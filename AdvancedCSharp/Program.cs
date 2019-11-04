b73a9016d78b78dad06f6b0a7082acd1b3125187
Ljourn Llagas <40813696+ljournllagas@users.noreply.github.com>
2019-11-04 10:45:47 +0800

    Add project files.

---
+++ AdvancedCSharp/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace AdvancedCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Nullable<int>();
+            Console.WriteLine("Has value: " + number.HasValue);
+            Console.WriteLine("Value: " + number.GetValueOrDefault());
+        }
+    }
+
+
+    #region "Using Delegates"
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }
+
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+
+    #endregion
+
+    #region "Using Generics"
+
+    // where T : struct
+    public class Nullable<T> where T : struct
+    {
+        private object _value;
+
+        public Nullable()
+        {
+
+        }
+
+        public Nullable(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+                return (T) _value;
+
+            return default;
+        }
+    }
+
+    // where T : Product
+    // where T : class
+    public class DiscountCalculator<TProduct> where TProduct : Product
+    {
+        public float CalculateDiscount(TProduct product)
+        {
+            return product.Price;
+        }
+    }
+
+    public class Book : Product
+    {
+        public string Isbn { get; set; }
+    }
+
+    public class Product
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+    }
+
+    // where T : new()
+    // where T : IComparable
+    public class Utilities<T> where T : IComparable, new()
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public T Max(T a, T b) 
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
+        }
+    }
+
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+
+    public class GenericList<T>
+    {
+        
+
+        public void Add(T value)
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException();}
+        }
+    }
+    #endregion
+
+}
